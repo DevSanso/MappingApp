49b661951ef71129fa26ddb3832f337cabd00cfe
Sunken Ahn <devsanso@outlook.com>
2022-07-26 13:22:09 +0900

    버퍼 변경, 쓰기,읽기등 최소한 기능들만  기능하도록 변경 -  #6

--- service/common/buffer/buffer.go
+++ service/common/buffer/buffer.go
@@ -2,69 +2,50 @@ package buffer
 
 import (
 	"sync"
-	"time"
 )
 
 
-
-
 type Buffer[T any] struct {
-	wChannel chan T
-	rChannel chan T
-	ticker time.Ticker
+	writeBuffer []T
+	readBuffer []T
 
 	mtx sync.RWMutex
 
 }
 
-func NewBuffer[T any](tick time.Duration) *Buffer[T] {
+func NewBuffer[T any]() *Buffer[T] {
 	buf := new(Buffer[T])
-	buf.ticker = *time.NewTicker(tick)
-	go buf.tickerHandle(buf.swapBuffer)
-
 	return buf
 }
 
 func (b *Buffer[T])swapBuffer() {
 	b.mtx.Lock()
 	b.mtx.RLock()
-	temp := b.wChannel
-	b.wChannel = b.rChannel
-	b.rChannel = temp
-	b.mtx.Unlock()
-	b.mtx.RUnlock()
+	defer b.mtx.Unlock()
+	defer b.mtx.RUnlock()
+
+	temp := b.writeBuffer
+	b.writeBuffer = b.readBuffer
+	b.readBuffer = temp
 }
 
-func (b *Buffer[T])tickerHandle(fn func()) {
-	for {
-		select {
-		case <-b.ticker.C:
-			fn()
-		default:
-			continue
-		}
-	}
-}
+
 
 func (b *Buffer[T])Push(data T) {
 	b.mtx.Lock()
-	b.wChannel <- data
-	b.mtx.Unlock()
+	defer b.mtx.Unlock()
+
+	b.writeBuffer = append(b.writeBuffer,data)
 }
 
 
-func (b *Buffer[T])copyRChannelThread(other chan T) {
-	for {
-		b.mtx.RLock()
-		data,ok := <- b.rChannel
-		if !ok {panic("buffer copyRChannelThread panic")}
-		other <- data
-		b.mtx.RUnlock()
-	}
+func (b *Buffer[T])Read() []T {
+	b.mtx.RLock()
+	defer b.mtx.RUnlock()
+
+	var res []T
+	copy(res,b.readBuffer)
+	b.readBuffer = b.readBuffer[:0]	
+	return res
 }
 
-func (b *Buffer[T])ReadChannel() <- chan T {
-	other := make(chan T)
-	go b.copyRChannelThread(other)
-	return other
-}
