d3e83b17be180a28262680ead8911cf8ee860598
Sunken Ahn <devsanso@outlook.com>
2022-07-26 13:57:54 +0900

    컨슈터 제너릭은 모든 타입이 가능,프로듀서 if를 함수로 사용하는 문법적 오류 수정, 컨슈머,프로듀서 모두 private,요구사항 해결 - #7

--- service/common/kafka/consumer_config.go
+++ service/common/kafka/consumer_config.go
@@ -5,15 +5,15 @@ import (
 	module "github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
-type ConsumerConfig[T ByteEncodeAndWriter] struct{
+type ConsumersConfig[T any] struct{
 	Address string
 	GroupID string
 	Offset string
 	Count int
-	EncodeNewFunc func() T
+	EncodeMessageFunc func([]byte) (T,error)
 }
 
-func (c *ConsumerConfig[T])toConfigMap() *module.ConfigMap {
+func (c *ConsumersConfig[T])toConfigMap() *module.ConfigMap {
 	return &module.ConfigMap{
 		"bootstrap.servers": c.Address,
 		"group.id":          c.GroupID,
