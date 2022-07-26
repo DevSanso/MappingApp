15e63e0bc75d4513db8df9b488414a2f282d9261
Sunken Ahn <devsanso@outlook.com>
2022-07-26 15:35:01 +0900

    프로듀서 재너릭 불필요로, 모두 제거 -  #7

--- service/common/kafka/producer.go
+++ service/common/kafka/producer.go
@@ -4,52 +4,61 @@ import (
 	module "github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
+type producerMessage interface {
+	Key() []byte
+	Topic() string
+	Message() []byte
+}
 
-
-
-type producer[T ByteDecoder] struct {
+type producer struct {
 	origin *module.Producer
 }
 
-
-func newProducer[T ByteDecoder](config *ProducerConfig)producer[T] {
-	p,err := module.NewProducer(config.toConfigMap())
-	if err != nil {panic(err)}
-	return producer[T]{origin : p}
+func newProducer(config *ProducerConfig) producer {
+	p, err := module.NewProducer(config.toConfigMap())
+	if err != nil {
+		panic(err)
+	}
+	return producer{origin: p}
 }
 
-
-func makeMessage[T ByteDecoder](key []byte,topic string,data T) (*module.Message,error) {
-	d,err := data.Decode()
-
-	if err != nil {return nil,err}
+func makeMessage(key []byte, topic string, data producerMessage) (*module.Message, error) {
+	d := data.Message()
 
 	if key == nil {
 		return &module.Message{
-			Value : d,
-			TopicPartition: module.TopicPartition{Topic: &topic,Partition: module.PartitionAny},
-		},nil
+			Value:          d,
+			TopicPartition: module.TopicPartition{Topic: &topic, Partition: module.PartitionAny},
+		}, nil
 	}
 	return &module.Message{
-		Value : d,
-		TopicPartition: module.TopicPartition{Topic: &topic,Partition: module.PartitionAny},
-		Key: key,
-	},nil
+		Value:          d,
+		TopicPartition: module.TopicPartition{Topic: &topic, Partition: module.PartitionAny},
+		Key:            key,
+	}, nil
 }
 
-func sendMessage[T ByteDecoder](origin *module.Producer,key []byte,topic string,data T)error {
-	message,err := makeMessage(key,topic,data)
-	if err != nil {return err}
+func sendMessage(origin *module.Producer, key []byte, topic string, data producerMessage) error {
+	message, err := makeMessage(key, topic, data)
+	if err != nil {
+		return err
+	}
 	return origin.Produce(message, nil)
 }
 
-func (p *producer[T])Send(topic string,data []T,key []byte) (err error) {
-	for _,message := range data {
-		err = sendMessage(p.origin,key,topic,message)
-		if err != nil {return}
+func (p *producer) Send(data []producerMessage) (err error) {
+	for _, message := range data {
+		err = sendMessage(p.origin, message.Key(), message.Topic(), message)
+		if err != nil {
+			return
+		}
 	}
 	return
 }
 
-func (p *producer[T])Close() {p.origin.Close()}
+func (p *producer) SendOne(data producerMessage) (err error) {
+	err = sendMessage(p.origin, data.Key(), data.Topic(), data)
+	return
+}
 
+func (p *producer) Close() { p.origin.Close() }
